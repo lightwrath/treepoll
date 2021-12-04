ea73e83afbbaef6d601c2d499ab1f1a9364a0ec0
lightwrath <lightwrath@live.com>
2021-12-04 03:46:29 +0200

    index welcome page outline

--- client/src/components/AppMainWindow.js
+++ client/src/components/AppMainWindow.js
@@ -1,8 +1,15 @@
 import React from "react"
+import config from "../config"
 
 export default function AppMainWindow({children}) {
   return (
-    <div>
+    <div
+      style={{
+        backgroundColor: config.background,
+        padding: "10px 50px",
+        borderRadius: "10px"
+      }}
+    >
       {children}
     </div>
   )
